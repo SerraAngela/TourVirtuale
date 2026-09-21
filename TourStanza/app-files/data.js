05cd45981945bdea94c8c1b59b292dd86c2212b0
SerraAngela <serra.angela85@gmail.com>
2026-09-21 16:05:02 +0200

    Add files via upload

---
+++ TourStanza/app-files/data.js
@@ -0,0 +1,34 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-whatsapp-image-2026-09-21-at-153913",
+      "name": "WhatsApp Image 2026-09-21 at 15.39.13",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "yaw": -1.0004754482013318,
+        "pitch": 0.04402146448664368,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
